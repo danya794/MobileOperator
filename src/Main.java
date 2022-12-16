d7d82b6c3b85c23629c64963c5cb7b035a5f56c3
Danil Kovalev <dannykvlv@gmail.com>
2022-12-16 13:27:37 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
             balance = balance + clientOrder1;
         }
         System.out.println(balance);
-        System.out.println(""clientOrder1);
 
         if (clientOrder2 == 1100) {
            countBonus=clientOrder2 / 100;
@@ -19,23 +18,5 @@ public class Main {
 
         balance=+clientOrder2+clientOrder1+countBonus;
         System.out.println(balance);
-
-
-
-
-
-      /*  int amount = 10000;
-        boolean isBought = true;
-        int miles;
-        if (isBought) {
-            miles = 500;
-        } else {
-            miles = 1;
-        }
-        int bonus = amount / 20;
-
-        System.out.println("Количество начисленных миль за купленный билет:" + bonus);
-
-        */
     }
 }
