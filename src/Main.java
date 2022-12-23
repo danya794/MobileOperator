f22001507c04107a671b9d65663a3b86cccc8325
Danil Kovalev <dannykvlv@gmail.com>
2022-12-23 09:25:33 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int clientOrder1=100;
-        int clientOrder2=1100;
-        int countBonus=0;
+        int clientOrder1 = 100;
+        int clientOrder2 = 1100;
+        int countBonus = 0;
 
 
         if (clientOrder1 == 100) {
@@ -13,10 +13,10 @@ public class Main {
         System.out.println(balance);
 
         if (clientOrder2 == 1100) {
-           countBonus=clientOrder2 / 100;
+            countBonus = clientOrder2 / 100;
         }
 
-        balance=+clientOrder2+clientOrder1+countBonus;
+        balance = +clientOrder2 + clientOrder1 + countBonus;
         System.out.println(balance);
     }
 }
