b7955d3373ce835881f55612aeee52254673b5f2
Danil Kovalev <dannykvlv@gmail.com>
2022-12-26 12:30:18 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int initialAccount = 100;
         int added = 1100;
 
-        int bonus = added > 1000 ? added / 100 :0;
+        int bonus = added > 1000 ? added / 100 : 0;
         int finalAccount = initialAccount + added + bonus;
 
         System.out.println("Итоговый счёт: " + finalAccount);
